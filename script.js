96ef8571bf4df275b6a0125316d1ef5099315419
farangiz77 <fireer4878@gmail.com>
2024-07-30 16:48:27 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+
+// let i = 0
+// while(i < 5) {
+//   console.log(i);
+//   i++
+// }
+
+
+// let i = 0
+// do{
+// console.log(i);
+// i++
+// }while(i < 10)
+ 
+
+
+// function sayHello(name) {
+//   console.log("hello " + name);
+// }
+
+// sayHello("fr")
+
+
+
+
+//  
+
+
+// const greet = function(a) {
+//   console.log("hello " + a );
+// }
+
+// greet("fr")
+// greet("rh")
+
+
+
+
+//arrow function
+// const plus = (a, b) => a + b
+// const minus = (a, b) => a - b
+// const delenie = (a, b) => a / b
+
+// console.log(plus(5, 5));
+// console.log(minus(5, 5));
+// console.log(delenie(5, 5));
+
+
+// const me = function(name, surname, age) {
+//   console.log("privet menya  zovut " + name +  surname + "mne " + age + " let ");
+
+// }
+
+const me = function(name, surname, age) {
+  
+console.log(`privet menya zovut ${name} ${surname}, mne ${age} let `);
+}
+
+
+me("farangiz  ", "rikhsiboyeva  ", 18)
+
+
+
